eb23161b9f503b36b442323e3fe23587ddbe6215
Ashwin Surajbali <ashwin@teedot.co>
2013-11-18 12:48:15 -0500

    Unable to verify access token temporary fix

--- LinkedIn/LinkedIn.php
+++ LinkedIn/LinkedIn.php
@@ -99,8 +99,11 @@
 	        	'client_secret' => $this->_config['api_secret'],
 	        	'redirect_uri' => $this->_config['callback_url']
 	        );
- 
-	        $data = $this->_makeRequest(self::OAUTH_BASE . '/accessToken', $params, 'POST', array('x-li-format: json'));
+	        
+	        /** Temp bug fix as per https://developer.linkedin.com/comment/28938#comment-28938 **/
+	        $tmp_params = http_build_query($params);
+
+	        $data = $this->_makeRequest(self::OAUTH_BASE . '/accessToken?' . $tmp_params, array(), 'POST', array('x-li-format: json'));
 	        if (isset($data['error']) && !empty($data['error'])){
 	            throw new \RuntimeException('Access Token Request Error: ' . $data['error'] . ' -- ' . $data['error_description']);
 	        }
